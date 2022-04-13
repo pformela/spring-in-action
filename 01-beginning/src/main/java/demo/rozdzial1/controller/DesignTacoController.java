d42e889fe688f01bbd02a888967cdbfd6d764191
Patryk Formela <p.formela.030@studms.ug.edu.pl>
2022-04-13 13:52:05 +0200

    objects and design page

---
+++ 01-beginning/src/main/java/demo/rozdzial1/controller/DesignTacoController.java
@@ -0,0 +1,55 @@
+package demo.rozdzial1.controller;
+
+import demo.rozdzial1.data.Ingredient;
+import demo.rozdzial1.data.Taco;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import demo.rozdzial1.data.Ingredient.Type;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList (
+                new Ingredient("FLTO", "pszenna", Type.WRAP),
+                new Ingredient("COTO", "kukurydziana", Type.WRAP),
+                new Ingredient("GRBF", "mielona wołowina", Type.PROTEIN),
+                new Ingredient("CARN", "kawałki mięsa", Type.PROTEIN),
+                new Ingredient("TMTO", "pomidory pokrojone w kostkę", Type.VEGGIES),
+                new Ingredient("LETC", "sałata", Type.VEGGIES),
+                new Ingredient("CHED", "cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "pikantny sos pomidorowy", Type.SAUCE),
+                new Ingredient("SRCR", "śmietana", Type.SAUCE)
+        );
+
+        Type[] types = Ingredient.Type.values();
+
+        for(Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
+        }
+
+        model.addAttribute("design", new Taco());
+
+        return "design";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+}
